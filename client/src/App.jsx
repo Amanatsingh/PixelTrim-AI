ccbaae65740d96488d245eda9e1ebc970c48018d
Amanat <amanatsingh630@gmail.com>
2025-07-04 20:21:31 +0530

    feat: Add SVG assets for icons and images, implement main components for homepage layout
    - Added SVG assets for React, remove background, slide, Twitter, upload button, and upload icons.
    - Created BgSlider component for interactive image comparison.
    - Implemented Footer, Header, Navbar, Steps, Testimonials, and Upload components for homepage structure.
    - Developed BuyCredit and Result pages as placeholders.
    - Configured Tailwind CSS and Vite for styling and build process.

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import NavBar from './components/NavBar'
+
+const App = () => {
+  return (
+    <div className='min-h-screen bg-slate-50'>
+      {/* created 3 routes for 3 pages */}
+      <NavBar />                         {/* so as it appears on all pages */}
+      <Routes>
+        <Route path="/" element={<Home></Home>} /> 
+        <Route path="/buy" element={<div>Buy Credit</div>} />
+        <Route path="/result" element={<div>Result</div>} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/pages/Result.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function result() {
+  return (
+    <div className='mx-4 my-3 lg:mx-44 mt-14 min-h-[75vh]'>
+      
+    </div>
+  )
+}
+
+export default result
