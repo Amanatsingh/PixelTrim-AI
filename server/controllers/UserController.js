0d0aa79f6c911d7f81404e402910378b8f2c0592
Amanat <amanatsingh630@gmail.com>
2025-07-14 08:13:50 +0530

    webhooks Added

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,7 +1,7 @@
 //NOTES:
 
 //share the data with backend, user creates account - we can store the user data in our database => use clerk webhook
-//whenever we'll create account we will get this type of data using web hooks -> using this data we can save in our Database
+//whenever we'll create account we will get this type of data(code below) using web hooks -> using this data we can save in our Database
 // {
 //   "event": "payment_success",
 //   "data": {
@@ -12,10 +12,20 @@
 // }
 
 // --------------
+
+import { Webhook } from 'svix'
+
 //API Controller Function to manage Clerk User with Database
 //API end point(created at) -  https://localhost:4000/api/user/webhooks
+
 const clerkWebhooks = async (req,res) => {
-    //logic: whenever we'll create a new user in frontend, we will get the user data in backend too
+    //logic: whenever we'll create a new user in frontend, we will get the user data in backend too -
     //we need public IP to work with clerk webhooks, we can use vercel to get public IP that will assign domain to our backend, we'll use this domain as public IP in clerk webhooks
+    try {
+        //
+        
+    } catch (error) {
+        
+    }
     
 }
